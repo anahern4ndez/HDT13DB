cb1a29fd0fd6ceb43dc9da69ef995cef2b2cb40d
Francisco Molina <molinajimenez01@gmail.com>
2019-05-07 11:56:08 -0600

    terminado el inciso 4 y 5 de la parte 1

--- lab13.sql
+++ lab13.sql
@@ -50,37 +50,46 @@ AS (
 
 
 
--- inciso d. ii
-SELECT genero, COUNT(*) as Conteo
-FROM vistaVentas
-WHERE vistaVentas.anio = 2013;
-GROUP BY genero, COUNT(*)
-ORDER BY COUNT(*)
+
 
 Select * from vistaVentas
 
 
---inciso d.i
+--inciso d.i -> mpeg
 select count(vistaVentas.mediatype), vistaVentas.mediatype
 from vistaVentas
 group by vistaVentas.mediatype
 order by count(vistaVentas.mediatype) desc
 limit 1;
 
---inciso d.ii
-SELECT vistaventas.genero, COUNT(*) as Conteo
+
+
+--	inciso d.ii -> rock mas vendido
+SELECT genero, COUNT(*) as Conteo
 FROM vistaVentas
 WHERE vistaVentas.anio = 2013
-GROUP BY vistaventas.genero
-ORDER BY COUNT(*) DESC
+GROUP BY genero
+ORDER BY COUNT(*) DESC;
 
--- 				inciso d. iii
-
+--	inciso d. iii
 SELECT semana, sum(total)
 FROM vistaVentas
 WHERE pais is null and ciudad is null and anio is null and cuarto is null and mes is null
 GROUP BY semana
 ORDER BY semana
 
--- 			inciso d.v
+--	inciso d. iv -> mejor es null, segundo mejor es 2.
+SELECT cuarto, SUM(total) AS totalCuarto 
+FROM vistaVentas
+GROUP BY cuarto
+ORDER BY totalCuarto DESC
 
+-- 			inciso d.v
+/*
+Debido a que existe una brecha de ventas de 3072 unidades entre musica 
+Rock y Jazz, que son el primer y segundo genero mas vendido respectivamente,
+creemos oportuno enfocar una estrategia de marketing hacia el genero Jazz. 
+
+Debido a que podemos conocer las semanas y quarters donde existen menor cantidad de ventas, siendo la semana 43
+respectiva al quarter 4. Podemos intensificar una campaña de marketing a este genero, en esta semana en concreto.
+*/
